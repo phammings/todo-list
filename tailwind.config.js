4f13613605c21707e6e47b90228d4bae76fb15c7
Ryan <ryanlepham9@gmail.com>
2023-08-31 21:36:55 -0400

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./dist/index.html",
+      "./src/style.js"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }

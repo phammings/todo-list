3af054c493f06341dd35d10d3aa8ed5438c51647
Ryan <ryanlepham9@gmail.com>
2023-09-08 19:53:01 -0400

    refactored ame

---
+++ src/modules/storage.ts
@@ -0,0 +1,13 @@
+import {Task} from "src/modules/Task";
+
+function saveTasks(tasks: Task[], projectName: string) {
+    localStorage.setItem(projectName, JSON.stringify(tasks));
+}
+  
+function loadTasks(projectName: string): Task[] {
+    const taskJSON = localStorage.getItem(projectName);
+    if (taskJSON == null) return [];
+    return JSON.parse(taskJSON);
+}
+
+export {saveTasks, loadTasks};

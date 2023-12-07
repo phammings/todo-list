d5f952f43b5d3ee2d959b7f70e66d037fdbaabc4
Ryan <ryanlepham9@gmail.com>
2023-12-06 22:06:06 -0500

    Added loadAllTasks, loadTodaysTasks, and loadWeeksTasks and fixed deleteTask()

--- src/modules/storage.ts
+++ src/modules/storage.ts
@@ -5,6 +5,16 @@ function saveTasks(tasks: Task[], projectName: string) {
 }
   
 function loadTasks(projectName: string): Task[] {
+    const allProjectNames = getAllProjectNames();
+    if (projectName == "All Tasks") {
+        return loadAllTasks(allProjectNames);
+    }
+    else if (projectName == "Today's Tasks") {
+        return loadTodaysTasks(allProjectNames);
+    }
+    else if (projectName == "This Week's Tasks") {
+        return loadThisWeeksTasks(allProjectNames);
+    }
     const taskJSON = localStorage.getItem(projectName);
     if (taskJSON == null) return [];
     return JSON.parse(taskJSON);
@@ -12,8 +22,7 @@ function loadTasks(projectName: string): Task[] {
 
 function deleteTask(projectName: string, taskName: Task) {
     let tasks: Task[] = loadTasks(projectName);
-    const taskIndex = tasks.findIndex((task) => task.title === taskName.title);
-    tasks.splice(taskIndex, 1);
+    tasks = tasks.filter((task) => task.title !== taskName.title)
     saveTasks(tasks, projectName);
 }
 
@@ -31,6 +40,76 @@ function deleteProject(projectName: string) {
     let projects = loadProjects();
     projects = projects.filter(project => project !== projectName);
     saveProject(projects);
+}
+
+function loadAllTasks(projectNames: string[]): Task[] {
+    const allTasks: Task[] = [];
+
+    projectNames.forEach(projectName => {
+        const taskJSON = localStorage.getItem(projectName);
+        if (taskJSON != null) {
+            const tasks = JSON.parse(taskJSON) as Task[];
+            allTasks.push(...tasks);
+        }
+    });
+
+    return allTasks;
+}
+
+function loadTodaysTasks(projectNames: string[]): Task[] {
+    const today = new Date();
+    today.setUTCHours(0, 0, 0, 0);
+
+    const todayTasks: Task[] = [];
+
+    projectNames.forEach(projectName => {
+        const taskJSON = localStorage.getItem(projectName);
+
+        if (taskJSON != null) {
+            const tasks = JSON.parse(taskJSON) as Task[];
+            const tasksToday = tasks.filter(task => {
+                const taskDueDate = new Date(task.dueDate);
+                taskDueDate.setUTCHours(0, 0, 0, 0);
+                return taskDueDate.getTime() === today.getTime();
+            });
+
+            todayTasks.push(...tasksToday);
+        }
+    });
+
+    return todayTasks;
+}
+
+function loadThisWeeksTasks(projectNames: string[]): Task[] {
+    const today = new Date();
+    today.setUTCHours(0, 0, 0, 0);
+
+    const endOfWeek = new Date(today);
+    endOfWeek.setDate(today.getDate() + (6 - today.getDay()));
+
+    const thisWeekTasks: Task[] = [];
+
+    projectNames.forEach(projectName => {
+        const taskJSON = localStorage.getItem(projectName);
+
+        if (taskJSON != null) {
+            const tasks = JSON.parse(taskJSON) as Task[];
+            const tasksThisWeek = tasks.filter(task => {
+                const taskDueDate = new Date(task.dueDate);
+                taskDueDate.setUTCHours(0, 0, 0, 0);
+                return taskDueDate >= today && taskDueDate <= endOfWeek;
+            });
+
+            thisWeekTasks.push(...tasksThisWeek);
+        }
+    });
+
+    return thisWeekTasks;
+}
+
+function getAllProjectNames(): string[] {
+    const allProjectNames = Object.keys(localStorage);
+    return allProjectNames;
 }
 
 export {saveTasks, loadTasks, saveProject, loadProjects, deleteProject, deleteTask};
